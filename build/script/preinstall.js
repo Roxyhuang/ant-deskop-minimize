64e078b6ddb21263754d84eca48c9f727c3fae2e
roxy_huang <sixin_huang@outlook.com>
2018-01-23 14:49:53 +0800

    update md5 for preinstall

--- build/script/preinstall.js
+++ build/script/preinstall.js
@@ -1,33 +1,66 @@
-const preList = require('../../package.json');
-
 const path = require('path');
+
+const fs = require('fs');
 
 const exec = require('child_process').exec;
 
+const preList = require('../../package.json');
+
+const { md5 } = require('../utils/cryptoUtils');
+
 const execPath = path.resolve('../');
 
-const obj = preList.devDependenciesGlobal;
+const preListObj = preList.devDependenciesGlobal;
 
-let script = `npm install -d --prefix ${execPath} -S`;
+const preListObjArray = Object.keys(preListObj);
 
-Object.keys(obj).forEach(function (name, index) {
-  script = `${script} ${name}@${obj[name]}`;
-});
+const isFirst = !fs.existsSync(`${execPath}/version.json`);
 
-console.log('start install public package for neo-antd');
+const currentMd5 = md5(JSON.stringify(preListObj));
 
-console.log(`will install in ${execPath}`);
+let script = `npm install -d --prefix ${execPath}  -S`;
 
-console.log('please wait some minutes.....');
+let isUpdate = false;
 
-exec(script, function (err, stdout, stderr) {
-  console.log(stdout);
-  if (err) {
-    console.log('error:' + stderr);
-  } else {
-    console.log(stdout);
-    console.log('package init success');
-    console.log(`The global install in ${execPath}`);
+console.log(`The md5 for this project: ${currentMd5}`);
+
+if (isFirst){
+  console.log('init global modules...');
+  fs.writeFileSync(`${execPath}/version.json`, JSON.stringify({"md5": currentMd5}));
+  isUpdate = true;
+} else {
+  console.log('will check update');
+  const lastVersion = require(`${execPath}/version.json`).md5;
+  if (lastVersion !== currentMd5) {
+    fs.writeFileSync(`${execPath}/version.json`, JSON.stringify({ "md5": currentMd5 }));
+    isUpdate = true;
   }
-});
+}
+
+if (isUpdate) {
+  console.log(`will install in ${execPath}`);
+  console.log('start install public package for neo-antd');
+  console.log('please wait some minutes.....');
+  preListObjArray.forEach(function (name, index) {
+    script = `${script} ${name}@${preListObj[name]}`;
+  });
+
+  exec(script, function (err, stdout, stderr) {
+    console.log(stdout);
+    if (err) {
+      console.log('error:' + stderr);
+    } else {
+      console.log(stdout);
+      console.log('package init success');
+      console.log(`The global install in ${execPath}`);
+    }
+  });
+
+} else {
+  console.log('not any update');
+  console.log('will be run next');
+}
+
+
+
 
